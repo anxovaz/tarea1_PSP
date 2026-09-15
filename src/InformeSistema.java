bc40e470d60a84ed5a747b2ce9545aad3b72289f
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-09-15 10:19:44 +0200

    Agregar JavaDoc y prints para memoria

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -1,13 +1,32 @@
+/**
+ * Clase que devuelve datos sobre el sistema, como RAM, CPU, etc.
+ *
+ * @author Anxo Vázquez
+ */
 public class InformeSistema {
+    /**
+     * Devuelve el número de procesadores
+     * @return numero de hilos
+     */
     public static int mostrarProcesadores(){
-        int numcores = Runtime.getRuntime().availableProcessors();
-        return numcores;
+        int cpus = Runtime.getRuntime().availableProcessors();
+        return cpus;
     }
 
-
+    /**
+     * Programa principal
+     * @param args
+     */
     public static void main(String[] args){
         System.out.println("Práctica 1 - Anxo Vázquez");
-        System.out.println("Número de procesadores: " + InformeSistema.mostrarProcesadores());
+        System.out.println("Número de procesadores (son hilos): " + InformeSistema.mostrarProcesadores());
+
+        System.out.println("*+*************");
+
+        System.out.println("Memoria reservada:");
+        System.out.println("Memoria libre:");
+        System.out.println("Memoria en uso:");
+        System.out.println("Memoria máxima disponible:");
 
     }
 }
