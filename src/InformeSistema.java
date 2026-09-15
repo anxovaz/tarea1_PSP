7abc2b2703b759af262272016e58472f4cc90602
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-09-15 10:13:13 +0200

    Función mostrarProcesadores

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -1,5 +1,13 @@
 public class InformeSistema {
+    public static int mostrarProcesadores(){
+        int numcores = Runtime.getRuntime().availableProcessors();
+        return numcores;
+    }
+
+
     public static void main(String[] args){
         System.out.println("Práctica 1 - Anxo Vázquez");
+        System.out.println("Número de procesadores: " + InformeSistema.mostrarProcesadores());
+
     }
 }
