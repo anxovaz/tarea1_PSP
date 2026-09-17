00ddc89dd4c2a7838c833504c72b2622880ba1d4
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-09-17 14:08:23 +0200

    Agrego método mostrarMemoria

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -14,6 +14,17 @@ public class InformeSistema {
     }
 
     /**
+     * Hace un print del estado de la memoria en KB
+     */
+    public static void mostrarMemoria(){
+        //Estos métodos estan en bytes, por eso los divido entre 1000 para convertirlos a KB y que quede más simplificada
+        System.out.println("Memoria Libre: " + (Runtime.getRuntime().freeMemory() / 1000) + " KBytes");
+        System.out.println("Memoria total reservada: " + (Runtime.getRuntime().totalMemory() / 1000) + " KBytes");
+        System.out.println("Memoria en uso: " + ((Runtime.getRuntime().totalMemory() + Runtime.getRuntime().freeMemory())/ 1000) + " KBytes");
+        System.out.println("Memoria máxima disponible: " + (Runtime.getRuntime().maxMemory() / 1000) + " KBytes");
+    }
+
+    /**
      * Programa principal
      * @param args
      */
@@ -23,10 +34,6 @@ public class InformeSistema {
 
         System.out.println("*+*************");
 
-        System.out.println("Memoria reservada:");
-        System.out.println("Memoria libre:");
-        System.out.println("Memoria en uso:");
-        System.out.println("Memoria máxima disponible:");
-
+        InformeSistema.mostrarMemoria();
     }
 }
