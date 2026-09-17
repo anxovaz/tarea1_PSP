1d3a9e50a99d3415044c7bac4be1d611dff82ee9
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-09-17 19:46:31 +0200

    Arreglo calculo diferencia memoria

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -2,40 +2,47 @@
  * Clase que devuelve datos sobre el sistema, como RAM, CPU, etc.
  *
  * @author Anxo Vázquez
+ *
+ * @version 1.0
  */
 public class InformeSistema {
+
     /**
-     * Devuelve el número de procesadores
+     * Devuelve el número de procesadores (hilos)
      * @return numero de hilos
      */
     public static int mostrarProcesadores(){
-        int cpus = Runtime.getRuntime().availableProcessors();
-        return cpus;
+        return Runtime.getRuntime().availableProcessors();
     }
 
     /**
-     * Hace un print del estado de la memoria en KB
+     * Hace un print del estado de la memoria en KiB
      */
     public static void mostrarMemoria(){
-        //Estos métodos estan en bytes, por eso los divido entre 1000 para convertirlos a KB y que quede más simplificada
-        System.out.println("Memoria Libre: " + (Runtime.getRuntime().freeMemory() / 1000) + " KBytes");
-        System.out.println("Memoria total reservada: " + (Runtime.getRuntime().totalMemory() / 1000) + " KBytes");
-        System.out.println("Memoria en uso: " + ((Runtime.getRuntime().totalMemory() + Runtime.getRuntime().freeMemory())/ 1000) + " KBytes");
-        System.out.println("Memoria máxima disponible: " + (Runtime.getRuntime().maxMemory() / 1000) + " KBytes");
+        System.out.println("Memoria Libre: " + (Runtime.getRuntime().freeMemory() / 1024) + " KiB");
+        System.out.println("Memoria total reservada: " + (Runtime.getRuntime().totalMemory() / 1024) + " KiB");
+        System.out.println("Memoria en uso: " + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024) + " KiB");
+        System.out.println("Memoria máxima disponible: " + (Runtime.getRuntime().maxMemory() / 1024) + " KiB");
     }
 
     /**
-     * Hace un print de el sistema operatico y una ruta a un archivo dentro de mi carpeta home
+     * Devuelve la memoria en uso en bytes.
+     * @return memoria usada
+     */
+    public static long getBytesMemoriaUso(){
+        return Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+    }
+
+    /**
+     * Hace un print del sistema operativo y una ruta
      */
     public static void mostrarMultiplataforma(){
-        //Imprime tipo de sistema, version (del kernel en caso de linux) y arquitectura
         System.out.println("Sistema Operativo: " + System.getProperty("os.name") + " " + System.getProperty("os.version") + " " + System.getProperty("os.arch"));
-        //user.home para el directorio home y user.dir para el directorio de trabajo actual del usuario
         System.out.println("Ruta archivo informe.txt: " + System.getProperty("user.home") + "/psp/informe.txt");
     }
 
     /**
-     * Programa principal
+     * Lanzador principal
      * @param args
      */
     public static void main(String[] args){
@@ -44,20 +51,21 @@ public class InformeSistema {
 
         System.out.println("*+*************");
 
-        System.out.println("Informe antes de reservar memoria 64MiB de memoria:");
+        System.out.println("Informe antes de reservar 64MiB de memoria:");
         InformeSistema.mostrarMemoria();
-        long[] reservado = new long[8 * 1024 * 1024]; // 8 M x 8 bytes = 64 MiB
+        long memoriaUsadaAntes = InformeSistema.getBytesMemoriaUso();
+
+        long[] reservado = new long[8 * 1024 * 1024]; //64 MiB
+        reservado[0] = 1; // Para evitar que el recolector de basura limpie la variable
+
         System.out.println("---");
-        long[] reservado2 = reservado;
-        System.out.println("Informe después de reservar memoria 64MiB de memoria:");
-        reservado[0] = 1; //Para que el recolector de basura no borre el array
+        System.out.println("Informe después de reservar 64MiB de memoria:");
         InformeSistema.mostrarMemoria();
 
+        System.out.println("Diferencia: " + ((InformeSistema.getBytesMemoriaUso() - memoriaUsadaAntes) / (1024 * 1024)) + " MiB");
 
-        System.out.println("*+*************");
+        System.out.println("***************");
 
         InformeSistema.mostrarMultiplataforma();
-
-
     }
 }
